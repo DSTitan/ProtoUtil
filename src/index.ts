24f1ad383f4de10a5b1faefa68051370431750a4
Nathan Torrezao <72593433+DSTitan@users.noreply.github.com>
2023-05-16 13:45:11 -0400

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export const TrimString = (string: string, length: number, ellipsis: string = "..."): string => (string.length > length ? `${string.substr(0, length)}${ellipsis}` : string);
+declare type TrimStringUtil = (string: string, length: number, ellipsis?: string) => string;
 
-export default { TrimString };
+export const TrimString: TrimStringUtil = (string, length, ellipsis = "..."): string => (string.length > length ? `${string.substring(0, length)}${ellipsis}` : string);
+
+const Utility = { TrimString };
+export default Utility;
